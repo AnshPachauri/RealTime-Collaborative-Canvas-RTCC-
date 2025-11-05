9c44daaff63f99c0642692c785cacb2ecb07ba51
Ansh Kumar Pachauri <anshpachauri84@gmail.com>
2025-11-05 15:25:38 +0530

    folder-structure-setup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,17 @@ import express from 'express';
 
 const app = express();
 
+
+
+app.get('/signup', (req, res) => {
+    res.send('This is a signup point');
+});
+
+app.get('signin', (req, res) => {
+    res.send('This is a signin point');
+});
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
